47a9d4b52cb3d56e55f892fc638e7cdd6a49e3b7
Johnmond Ghaisen <117686102+lloduye@users.noreply.github.com>
2022-11-14 15:17:58 -0400

    Created HTML file 2 and linked Javascript file with the new function and object

--- Basic_HTML_and_JavaScript_1/Basic_HTML_and_JavaScript_1.js
+++ Basic_HTML_and_JavaScript_1/Basic_HTML_and_JavaScript_1.js
@@ -1,1 +1,8 @@
 alert ("Hello, World! I am an alert box!");
+function My_First_Function() {
+        var Introduction = "Hi, I'm Johnmond and I'm a student currently persuing a Diploma in Web and Software Development at Academy of Learning!";
+        var result = Introduction.fontcolor("red");
+
+        document.getElementById("Greetings").innerHTML =
+        result;
+        }
